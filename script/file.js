b48619c80911c435ee86965139d7ab47d6288d37
WindrunnerMax <651525974@qq.com>
2023-07-15 20:47:08 +0800

    feat: manifest.json watcher

--- script/file.js
+++ script/file.js
@@ -1,4 +1,5 @@
 const thread = require("child_process");
+const path = require("path");
 
 const exec = command => {
   return new Promise((resolve, reject) => {
@@ -11,6 +12,10 @@ const exec = command => {
 
 class FilesPlugin {
   apply(compiler) {
+    compiler.hooks.make.tap("JsonWatcher", compilation => {
+      compilation.fileDependencies.add(path.join(__dirname, "../src/manifest.json"));
+    });
+
     compiler.hooks.done.tapPromise("FilePlugin", () => {
       return Promise.all([
         exec("cp ./src/manifest.json ./dist/"),
