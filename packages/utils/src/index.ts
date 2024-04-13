6f7ef57e58d4750100432c796304536e212fa6f9
WindrunnerMax <651525974@qq.com>
2024-04-13 22:48:59 +0800

    feat: initialize project

---
+++ packages/utils/src/index.ts
@@ -0,0 +1,17 @@
+export {
+  cs,
+  DateTime,
+  getUniqueId,
+  isArray,
+  isEmptyValue,
+  isFunction,
+  isNumber,
+  isObject,
+  isPlainNumber,
+  isString,
+  storage,
+  TSON,
+} from "laser-utils";
+export { default as debounce } from "lodash-es/debounce";
+// Compat: FireFox https://github.com/lodash/lodash/issues/3126
+export { default as throttle } from "lodash-es/throttle";
