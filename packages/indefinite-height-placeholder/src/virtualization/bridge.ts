8accc9ebd3c8a75a6d87bc917a280ac41b4c1ea7
WindrunnerMax <651525974@qq.com>
2024-04-14 22:33:33 +0800

    chore: rename package

--- packages/indefinite-height-placeholder/src/virtualization/bridge.ts
+++
@@ -1,6 +0,0 @@
-import type { Node } from "./node";
-
-export const BATCH = 10;
-export const THRESHOLD = 3;
-export const DEFAULT_HEIGHT = 60;
-export const ELEMENT_TO_NODE = new WeakMap<Element, Node>();
